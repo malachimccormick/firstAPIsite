c3b83f10456c9d3f08f392fc0f8e3b985f0524b7
Malachi Mccormick <malachimccormick@Malachis-MacBook-Pro.local>
2018-11-15 23:21:25 -0500

    ip location and weather based off location

--- apiScript.js
+++ apiScript.js
@@ -1,44 +1,39 @@
 //Getting the ip address location and using it to showcase the weather
 
-// $.get('https://ipapi.co/json/', function(data){
-//     const locationCity = data.city
+$.get('https://ipapi.co/json/', function(data){
+    const locationCity = data.city;
     
-//         const locationRegion = data.region
+        const locationRegion = data.region;
 
-//             $("#location").html("Your location is " + locationCity + ", " + locationRegion )
+            $("#location").html("Your location is " + locationCity + ", " + locationRegion )
 
-            $.get('https://www.census.gov/data/developers/data-sets.html', function(data){
-                // const locationCity = data.city
-                    console.log(data)
-                //     const locationRegion = data.region
-            
-                //         $("#location").html("Your location is " + locationCity + ", " + locationRegion )
+           
 
-// const showWeather = (data) => {
-//     console.log(data)
-//     const condition = data.current.condition.text
+const showWeather = (data) => {
+    console.log(data);
+    const condition = data.current.condition.text;
     
-//         const conditionPic = "http:" + data.current.condition.icon
+        const conditionPic = "http:" + data.current.condition.icon;
     
-//             const temp = data.current.temp_f
+            const temp = data.current.temp_f;
 
-//         console.log(condition)
-//             $("#weather").html(condition)
-//         $("#conditionPicture").attr('src', conditionPic)
-//             $("#temp").html("The current temperature is " + temp + " degrees farenheit")
-// }
-//         const city = locationCity
+        console.log(condition);
+            $("#weather").html(condition);
+        $("#conditionPicture").attr('src', conditionPic);
+            $("#temp").html("The current temperature is " + temp + " degrees farenheit");
+}
+        const city = locationCity;
 
-//             const apiKey = "505eb63b691b40d9b5f150200181311"
+            const apiKey = "505eb63b691b40d9b5f150200181311";
 
-//                 const weatherURL = "https://api.apixu.com/v1/current.json?key=" + apiKey + "&q=" + city
+                const weatherURL = "https://api.apixu.com/v1/current.json?key=" + apiKey + "&q=" + city;
 
-// console.log(weatherURL)
+console.log(weatherURL);
 
 
 
 $.ajax({
     url:weatherURL,
     success: showWeather
-}) 
-})
+}) ;
+});
