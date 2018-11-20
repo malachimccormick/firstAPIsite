733e34c8e46f23a040caf36d9786adb6b4ccbae6
Malachi Mccormick <malachimccormick@Malachis-MacBook-Pro.local>
2018-11-19 19:43:07 -0500

    sanatized address input

--- apiScript.js
+++ apiScript.js
@@ -6,15 +6,15 @@
 function ipAddress(data) {
     $.get('http://ip-api.com/json', function (data) {
         ipAddress = data.query
-            $(".ipAddress").html(ipAddress).hide().show(600)
-                console.log(data)
-                    $.ajax({
-                        url: "http://ip-api.com/json",
-                            success: function(data){ 
-                                city = data.city;
-                                    region = data.regionName 
-                                getWeather(city,region)
-                    
+        $(".ipAddress").html(ipAddress).hide().show(600)
+        console.log(data)
+        $.ajax({
+            url: "http://ip-api.com/json",
+            success: function (data) {
+                city = data.city;
+                region = data.regionName
+                getWeather(city, region)
+
             }
         })
     })
@@ -25,72 +25,124 @@ ipAddress()
 // then passes it's data to the map feature
 
 const apiKey = "505eb63b691b40d9b5f150200181311";
-    function getWeather(city,region) {
-        const weatherURL = "https://api.apixu.com/v1/current.json?key=" + apiKey + "&q="+ city + "," + region;
-//This is the part that shows the weather after it is gotten.
-//is it needed??? Not really but I like the practice
+
+function getWeather(city, region) {
+    const weatherURL = "https://api.apixu.com/v1/current.json?key=" + apiKey + "&q=" + city + "," + region;
+    //This is the part that shows the weather after it is gotten.
+    //is it needed??? Not really but I like the practice
     function showWeather(data) {
         console.log(data)
-            const condition = data.current.condition.text;
-                const conditionPic = "http://" + data.current.condition.icon;
-                    const temp = data.current.temp_f;
-                        const locationCity = data.location.name
-                            const locationRegion = data.location.region
-    
-                            $("#location").html("Your current location is " + locationCity + ", " + locationRegion)
-                        $("#weather").html(condition)
-                    $("#conditionPicture").attr('src', conditionPic)
-                $("#temp").html("The current teperature is " + temp + " degrees fahrenheit")
+        const condition = data.current.condition.text;
+        const conditionPic = "http://" + data.current.condition.icon;
+        const temp = data.current.temp_f;
+        const locationCity = data.location.name
+        const locationRegion = data.location.region
+
+        $("#location").html("Your current location is " + locationCity + ", " + locationRegion)
+        $("#weather").html(condition)
+        $("#conditionPicture").attr('src', conditionPic)
+        $("#temp").html("The current teperature is " + temp + " degrees fahrenheit")
     }
     $.ajax({
         url: weatherURL,
-            success: function(data){
-                showWeather(data)
-                    locate()
-         
+        success: function (data) {
+            showWeather(data)
+            locate()
+
         }
-    }) 
+    })
 }
 
 //This gets the constant lat and lng from the Google API and loads it up.
 const key = "AIzaSyAvJXppOn7Qu0Waom3aJRK1sigS0KKyVDY"
-function locate() {
-    
-        $.post(('https://www.googleapis.com/geolocation/v1/geolocate?key=' + key), function (data) {
-            console.log(data)
-                let pos = data.location
-                    console.log(pos)
-                        initMap(pos)
+
+function locate(pos) {
+
+    $.post(('https://www.googleapis.com/geolocation/v1/geolocate?key=' + key), function (data) {
+        console.log(data)
+        pos = data.location
+        console.log(pos)
+        initMap(pos)
     })
 
 }
 
 //Making the map and putting it on the page. I tried jquery at the map var but I could not get the map to work right.
-    let map;
-    //Click function added so you can enter your own address and the map goes there.
-    //100% accurate when it comes to the location
-        $("#submit").click(function(){
-            address = $("#input_address").val();
-            console.log(address)
-                $.get(('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + key), function (data) {
-                    console.log(data)
-                        pos = data.results[
-                            0 ].geometry.location
-                        initMap(pos)
-            })
-    })
+let map;
+//Click function added so you can enter your own address and the map goes there.
+//100% accurate when it comes to the location
+$("#submit").click(function () {
+    address = $("#input_address").val();
 
-    function initMap(pos) {
+    console.log(address)
+
+    $.get(('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + key), function (data) {
+        console.log(data)
+
+        if ((data.results[0].address_components).length == 8){
+            newCity = data.results[0].address_components[2].long_name
+            newRegion = data.results[0].address_components[4].long_name
+            pos = data.results[
+                0].geometry.location 
+        } else if ((data.results[0].address_components).length == 4) {
+            newCity = data.results[0].address_components[0].long_name
+            newRegion = data.results[0].address_components[2].long_name
+            pos = data.results[
+                0].geometry.location
+            // initMap(pos)
+        } else if ((data.results[0].address_components).length == 2) {
+            newCity = data.results[0].address_components[0].long_name
+            newRegion = data.results[0].address_components[1].long_name
+            pos = data.results[0].geometry.location
+            // initMap(pos)
+        } else {
+            newCity = data.results[0].address_components[3].long_name
+            newRegion = data.results[0].address_components[5].long_name
+            pos = data.results[
+                0].geometry.location
+
+        }
+
+        initMap(pos)
+        newWeather(newCity, newRegion)
+    })
+})
+
+function initMap(pos) {
     //Create the map
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: pos,
-                zoom: 17
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pos,
+        zoom: 17
     });
-    
+
     // Create a marker and set its position.
-     marker = new google.maps.Marker({
+    marker = new google.maps.Marker({
         map: map,
-            position: pos,
-                title: 'Hello World!'
+        position: pos,
+        title: 'Hello World!'
     });
+}
+
+function newWeather(newCity, newRegion) {
+    const weatherURL = "https://api.apixu.com/v1/current.json?key=" + apiKey + "&q=" + newCity + "," + newRegion;
+    //This is the part that shows the weather after it is gotten.
+    //is it needed??? Not really but I like the practice
+    function showWeather(data) {
+        console.log(data)
+        const newCondition = data.current.condition.text;
+        const newConditionPic = "http://" + data.current.condition.icon;
+        const newTemp = data.current.temp_f;
+        $("#address").hide()
+        $("#newConditionPicture").attr('src', newConditionPic)
+        $("#foundWeather").html(newCity + "'s weather is " + newCondition + " and the temperature is " + newTemp + "F").hide().show(600)
+
+    }
+    $.ajax({
+        url: weatherURL,
+        success: function (data) {
+            showWeather(data)
+
+
+        }
+    })
 }
